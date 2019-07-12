bbf255f0592d24ae03c2cedd7b148ebce1c1e042
wq <>
2019-07-12 22:43:29 +0800

    no message

---
+++ WXMComponentManager/WXMComponent/WXMComponentBridge.m
@@ -0,0 +1,13 @@
+//
+//  WXMComponentBridge.m
+//  Multi-project-coordination
+//
+//  Created by wq on 2019/7/12.
+//  Copyright © 2019年 wxm. All rights reserved.
+//
+
+#import "WXMComponentBridge.h"
+
+@implementation WXMComponentBridge
+
+@end
